c85a8cee7ec6aa249a161b0b3006daa94eec6a8b
thapadevendra <43578948+thapadevendra@users.noreply.github.com>
2019-09-27 20:29:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-# referenced https://leithsuheimat.wordpress.com/2017/11/22/simple-python-script-to-check-website-response-time/
-# referenced https://stackabuse.com/read-a-file-line-by-line-in-python/
+# referenced https://leithsuheimat.wordpress.com/2017/11/22/simple-python-script-to-check-website-response-time/                                                                                      
+# referenced https://stackabuse.com/read-a-file-line-by-line-in-python/                                                                                                                               
 import datetime
 import requests
+import json
 
 with open('ips.txt') as fp:
     line = fp.readline()
@@ -11,7 +12,12 @@ with open('ips.txt') as fp:
             r = requests.get(url, timeout=4)
             r.raise_for_status()
             respTime = str(round(r.elapsed.total_seconds(),4))
-            print(line.rstrip() + " " + respTime + " " + r.text)
+            if r.headers['content-type'] == "application/json":
+                data = json.loads(r.text)
+                retNum = str(data["returnNum"])
+            else:
+                retNum = r.text
+            print(line.rstrip() + " " + respTime + " " + retNum)
             line = fp.readline()
         except requests.exceptions.HTTPError as err01:
             print("HTTP error: ", err01)
@@ -21,3 +27,6 @@ with open('ips.txt') as fp:
             print ("Timeout error:", err03)
         except requests.exceptions.RequestException as err04:
             print("Error: ", err04)
+
+
+
